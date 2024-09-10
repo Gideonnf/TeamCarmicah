290b29d72c4640abf4d8c02643219116d5b087c3
Gideon Francis <gideonnfrancis999@gmail.com>
2024-09-10 23:36:27 +0800

    glfw runs kinda

--- Carmicah/Carmicah/source/Carmicah/Application.h
+++ Carmicah/Carmicah/source/Carmicah/Application.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "GLFW/glfw3.h"
 #include "CarmicahCore.h"
 namespace Carmicah
 {
@@ -9,7 +9,7 @@ namespace Carmicah
 	public:
 		Application();
 		virtual ~Application();
-		void run();
+		int run();
 	};
 }
 
