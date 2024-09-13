fc9263623d378f35ff89db1ef5083b1d1ee3cc88
Rain <47514829+NivleMW@users.noreply.github.com>
2024-09-13 10:30:46 +0800

    Cleannnn

--- Carmicah/Carmicah/source/Carmicah.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "Carmicah/Application.h"
--- Carmicah/Carmicah/source/Carmicah/CarmicahCore.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-// this was here cause i tried dynamic linking but i realise dynamic isnt needed
-
-//#ifdef CARMICAH_BUILD_DLL
-//	#define CARMICAH_API __declspec(dllexport)
-//#else
-//	#define CARMICAH_API __declspec(dllimport)
-//#endif
