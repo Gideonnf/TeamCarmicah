24b72d530d2c0ac12eb1935d0db9340172d6a709
Gideon Francis <gideonnfrancis999@gmail.com>
2024-10-03 04:57:41 +0800

    :pray: :es:

--- CarmicahEngine/Carmicah/source/Systems/GameLogic.cpp
+++ CarmicahEngine/Carmicah/source/Systems/GameLogic.cpp
@@ -51,10 +51,6 @@ namespace Carmicah
            // SystemManager::GetInstance()->GetSystem<SoundSystem>()->PauseResumeSound
         }
 
-        // TODO: Input key press is broken atm. Pressed will only work for the very first function call cause its a global map
-        // I need to make an event listener 
-        /*
-        // Trigger physics debug
         if (Input.IsKeyPressed(Keys::KEY_P))
         {
             physicsRef->mDebugPhysics = true;
@@ -66,19 +62,19 @@ namespace Carmicah
             physicsRef->mToggleUpdate = true;
         }
 
-        if (Input.IsKeyPressed(Keys::KEY_D))
+        if (physicsRef->mDebugPhysics ? Input.IsKeyPressed(Keys::KEY_D) : Input.IsKeyHold(Keys::KEY_D))
         {
             mainCharacter.GetComponent<RigidBody>().velocity.x = 5.0f;
         }
-        else if (Input.IsKeyPressed(Keys::KEY_A))
+        else if (physicsRef->mDebugPhysics ? Input.IsKeyPressed(Keys::KEY_A) : Input.IsKeyHold(Keys::KEY_A))
         {
             mainCharacter.GetComponent<RigidBody>().velocity.x = -5.0f;
         }
-        else if (Input.IsKeyPressed(Keys::KEY_W))
+        else if (physicsRef->mDebugPhysics ? Input.IsKeyPressed(Keys::KEY_W) : Input.IsKeyHold(Keys::KEY_W))
         {
             mainCharacter.GetComponent<RigidBody>().velocity.y = 5.0f;
         }
-        else if (Input.IsKeyPressed(Keys::KEY_S))
+        else if (physicsRef->mDebugPhysics ? Input.IsKeyPressed(Keys::KEY_S) : Input.IsKeyHold(Keys::KEY_S))
         {
             mainCharacter.GetComponent<RigidBody>().velocity.y = -5.0f;
         }
@@ -87,80 +83,7 @@ namespace Carmicah
             mainCharacter.GetComponent<RigidBody>().velocity.x = 0.0f;
             mainCharacter.GetComponent<RigidBody>().velocity.y = 0.0f;
         }
-       */
-        if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS && !pKeyWasPressed) {
-            physicsRef->mDebugPhysics = !physicsRef->mDebugPhysics;
-            pKeyWasPressed = true;  // Mark the key as pressed
-        }
-        else if (glfwGetKey(window, GLFW_KEY_P) == GLFW_RELEASE) {
-            pKeyWasPressed = false;  // Reset the key press flag when the P key is released
-        }
-
-        if (physicsRef->mDebugPhysics) {
-            // Handle WASD movement during debugPhysics mode
-            if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS && !tKeyWasPressed)
-            {
-                physicsRef->mToggleUpdate = true;
-                tKeyWasPressed = true;
-            }
-            else if (glfwGetKey(window, GLFW_KEY_T) == GLFW_RELEASE)
-            {
-                tKeyWasPressed = false;
-            }
-
-            // Check if a movement key was pressed
-            bool movementKeyPressed = glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS ||
-                glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS ||
-                glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS ||
-                glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS;
-
-            if (movementKeyPressed && !moveKeyWasPressed) {
-                physicsRef->mToggleUpdate = true;
-                moveKeyWasPressed = true;  // Mark movement key as pressed
-            }
-            else if (!movementKeyPressed) {
-                moveKeyWasPressed = false;  // Reset when no key is pressed
-            }
-        }
-
-        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        {
-            mainCharacter.GetComponent<RigidBody>().velocity.x = 5.0f;
-        }
-        else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        {
-            mainCharacter.GetComponent<RigidBody>().velocity.x = -5.0f;
-        }
-        else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        {
-            mainCharacter.GetComponent<RigidBody>().velocity.y = 5.0f;
-        }
-        else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        {
-            mainCharacter.GetComponent<RigidBody>().velocity.y = -5.0f;
-        }
-        else
-        {
-            mainCharacter.GetComponent<RigidBody>().velocity.x = 0.0f;
-            mainCharacter.GetComponent<RigidBody>().velocity.y = 0.0f;
-        }
-
 
         FPSText.GetComponent<TextRenderer>().txt = "FPS: " + std::to_string(static_cast<int>(CarmicahTimer::GetFPS()));
-        //if (Input.IsKeyPressed(Keys::KEY_SPACEBAR))
-        //{
-        //    sceneSys->ChangeScene(scene2Name);
-        //}
-
-        //if (Input.IsKeyPressed(Keys::KEY_1))
-        //{
-        //    GameObject duckObj = gGOFactory->FetchGO("Duck");
-        //    duckObj.Destroy();
-        //}
-
-        //if (Input.IsKeyPressed(Keys::KEY_P))
-        //{
-        //    souSystem->PauseResumeSound(souSystem->defaultBGM);
-        //}
 	}
 }
