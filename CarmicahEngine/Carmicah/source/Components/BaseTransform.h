502430c9550d496859230110298a9508ee063b38
Gideon Francis <gideonnfrancis999@gmail.com>
2024-11-01 15:18:01 +0800

    merged with rain peko :skull: :cry: :fire: :cry:

--- CarmicahEngine/Carmicah/source/Components/BaseTransform.h
+++ CarmicahEngine/Carmicah/source/Components/BaseTransform.h
@@ -23,7 +23,7 @@ namespace Carmicah
         {
             pos.x = static_cast<float>(component["xPos"].GetDouble());
             pos.y = static_cast<float>(component["yPos"].GetDouble());
-            depth = static_cast<float>(component["zPos"].GetDouble());
+            depth = static_cast<float>(component["depth"].GetDouble());
             scale.x = static_cast<float>(component["xScale"].GetDouble());
             scale.y = static_cast<float>(component["yScale"].GetDouble());
         }
@@ -34,7 +34,7 @@ namespace Carmicah
             writer.Double(pos.x);
             writer.String("yPos");
             writer.Double(pos.y);
-            writer.String("zPos");
+            writer.String("depth");
             writer.Double(depth);
             writer.String("xScale");
             writer.Double(scale.x);
