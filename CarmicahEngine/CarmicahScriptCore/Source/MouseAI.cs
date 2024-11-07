09fc881d65c505732f3faab313e3406044c15d1f
Gideon Francis <gideonnfrancis999@gmail.com>
2024-11-07 20:41:54 +0800

    working on enemy AI and state machines

---
+++ CarmicahEngine/CarmicahScriptCore/Source/MouseAI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carmicah
+{
+    public class MouseAI : Entity
+    {
+        StateMachine mouseSM;
+
+        void OnCreate()
+        {
+            MouseChase chaseState = new MouseChase("MouseChase");
+            MouseChase deadState = new MouseChase("MouseDead");
+
+            mouseSM.AddState(chaseState);
+            mouseSM.AddState(deadState);
+        }
+
+        void OnUpdate(float dt)
+        {
+            mouseSM.Update(ref mouseSM);
+
+            // If its dead, kill it
+            if (mouseSM.GetCurrentState() == "MouseDead")
+            {
+                // kill it
+            }
+        }
+
+    }
+}
