b5413592ab4c7a7f9c227baea101c82dd96bbfe2
Gideon Francis <gideonnfrancis999@gmail.com>
2024-12-01 21:57:59 +0800

    Fix cloning of UITransform :skull: :skull: :skull:

--- CarmicahEngine/Carmicah/source/Systems/GOFactory.cpp
+++ CarmicahEngine/Carmicah/source/Systems/GOFactory.cpp
@@ -116,27 +116,36 @@ namespace Carmicah
 		Carmicah::Log::GetCoreLogger()->info("Creating a clone of game object with name: " + newGO.mName + " with id: " + std::to_string(newGO.mID));
 
 		SystemManager::GetInstance()->UpdateSignatures(newGO.mID, EntityManager::GetInstance()->GetSignature(newGO.mID));
-		Transform& newTransform = ComponentManager::GetInstance()->GetComponent<Transform>(newGO.mID);
-		newTransform.children.clear(); // if they copy they'll copy the children list also
-
-		// Update parent will replace parent so dont have to reseet parent
-		// by default, if creating a new clone the value of parentID is 0,
-		// so it will be parented to the scene unless specified other wies by passing in a parent
-		UpdateParent(newGO.mID, parentID);
-
-		Transform& originalTransform = ComponentManager::GetInstance()->GetComponent<Transform>(go.mID);
-		// Check for children
-		if (originalTransform.children.size() > 0)
+		if (ComponentManager::GetInstance()->HasComponent<Transform>(newGO.mID))
 		{
-			// Clone the child and parent it to this
-			for (auto it : originalTransform.children)
+			Transform& newTransform = ComponentManager::GetInstance()->GetComponent<Transform>(newGO.mID);
+			newTransform.children.clear(); // if they copy they'll copy the children list also
+
+			// Update parent will replace parent so dont have to reseet parent
+			// by default, if creating a new clone the value of parentID is 0,
+			// so it will be parented to the scene unless specified other wies by passing in a parent
+			UpdateParent(newGO.mID, parentID);
+
+			Transform& originalTransform = ComponentManager::GetInstance()->GetComponent<Transform>(go.mID);
+			// Check for children
+			if (originalTransform.children.size() > 0)
 			{
-				GameObject newGo = gGOFactory->FetchGO(it);
-				// Clone this go and parent it to the cloned go
-				CloneGO(newGo, newGO.mID);
+				// Clone the child and parent it to this
+				for (auto it : originalTransform.children)
+				{
+					GameObject newGo = gGOFactory->FetchGO(it);
+					// Clone this go and parent it to the cloned go
+					CloneGO(newGo, newGO.mID);
+				}
 			}
 		}
+		// UITransform has no parent and child yet so we do it differently than Transform
+		else if (ComponentManager::GetInstance()->HasComponent<UITransform>(newGO.mID))
+		{
+			UpdateParent(newGO.mID, parentID);
 
+
+		}
 
 		return newGO;
 	}
