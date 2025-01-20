5fccd793c5ddb0a353675072b432f2c82015e999
yuxjay205 <yangyujie205@gmail.com>
2025-01-21 01:39:41 +0800

    progress-saving
    not working yet,smth is wrong

---
+++ CarmicahEngine/CarmicahScriptCore/Source/Projectile.cs
@@ -0,0 +1,56 @@
+﻿using Carmicah;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarmicahScriptCore.Source
+{
+    public class NPCProjectile : Entity
+    {
+        public float Speed = 5.0f;
+        public float LifeTime = 2.0f;
+        public string ProjectileHitAnim;
+        public string ProjectileMoveAnim;
+        public float timer = 0.0f;
+        public bool isMoving = true;
+
+        void OnCreate()
+        {
+            if (!string.IsNullOrEmpty(ProjectileMoveAnim))
+            {
+                ChangeAnim(ProjectileMoveAnim);
+            }
+        }
+
+        void OnUpdate(float dt)
+        {
+            Entity pauseManager = FindEntityWithName("PauseManager");
+            if (pauseManager != null)
+            {
+                if (pauseManager.As<PauseManager>().IsPaused)
+                    return;
+            }
+
+            timer += dt;
+            if (timer >= LifeTime)
+            {
+                Destroy();
+            }
+        }
+
+        void OnCollide()
+        {
+            if (isMoving)
+            {
+                isMoving = false;
+                if (!string.IsNullOrEmpty(ProjectileHitAnim))
+                {
+                    ChangeAnim(ProjectileHitAnim);
+                }
+                Destroy();
+            }
+        }
+    }
+}
