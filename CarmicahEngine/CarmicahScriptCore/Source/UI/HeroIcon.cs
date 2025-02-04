777bfee4e2ebd7b379892d56a5e67e59eb85b5e6
Gideon Francis <gideonnfrancis999@gmail.com>
2025-02-04 08:00:06 +0800

    Hero building :pensive: :skull: :ski: :fire_engine: :fire:

--- CarmicahEngine/CarmicahScriptCore/Source/UI/HeroIcon.cs
+++ CarmicahEngine/CarmicahScriptCore/Source/UI/HeroIcon.cs
@@ -8,9 +8,36 @@ namespace Carmicah
 {
     public class HeroIcon : Entity
     {
-        public void OnClick()
-        {
+        public string heroPrefab = "Trap_1";
+        public Entity heroEntity;
 
+        void OnUpdate(float dt)
+        {
+            if (heroEntity == null) return;
+
+            //CMConsole.Log("It shouldnt be here atm");
+
+            //if (IsKeyHold(Keys.))
+            Vector2 mousePos = Input.GetMousePos();
+            heroEntity.Position = mousePos;
+
+
+
+            if (Input.IsMouseReleased(MouseButtons.MOUSE_BUTTON_LEFT))
+            {
+                CMConsole.Log($"Destroying entity with {heroEntity.mID}");
+                heroEntity.Destroy();
+                heroEntity = null;
+            }
         }
+
+        void OnClick()
+        {
+            if (heroEntity != null) return;
+
+            heroEntity = CreateGameObject(heroPrefab);
+            CMConsole.Log($"Creating entity with {heroEntity.mID}");
+        }
+
     }
 }
