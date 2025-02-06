1422863f1ee988c014451a348c7ba83de55f53d9
Gideon Francis <gideonnfrancis999@gmail.com>
2025-02-07 07:44:12 +0800

    i forgot

---
+++ CarmicahEngine/CarmicahScriptCore/Source/TowerController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carmicah
+{
+    public class TowerController : Entity
+    {
+        public int towerHealth = 10;
+
+    }
+}
