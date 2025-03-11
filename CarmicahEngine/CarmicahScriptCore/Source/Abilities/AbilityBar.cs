fe961614ea1001a200a3a0bb12b87e2bb513f039
Gideon Francis <gideonnfrancis999@gmail.com>
2025-03-11 22:03:47 +0800

    Unprefabbed the mc and removed old hp bar

--- CarmicahEngine/CarmicahScriptCore/Source/Abilities/AbilityBar.cs
+++ CarmicahEngine/CarmicahScriptCore/Source/Abilities/AbilityBar.cs
@@ -32,11 +32,11 @@ namespace Carmicah
         public override void OnCreate()
         {
             CreateIcon(IconType.SPEAR_ICON);
-            CreateIcon(IconType.SPEAR_ICON);
+            CreateIcon(IconType.CANDY_ICON);
             CreateIcon(IconType.MAGE_ICON);
-            CreateIcon(IconType.MAGE_ICON);
+            CreateIcon(IconType.HONEY_ICON);
             CreateIcon(IconType.SHOOTER_ICON);
-            CreateIcon(IconType.SHOOTER_ICON);
+            CreateIcon(IconType.CANDY_ICON);
 
             originalPos = this.Position;
         }
