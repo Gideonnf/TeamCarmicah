b9f2f3d5396b12f27f0bd3e0a02e24a1f99d28ca
Gideon Francis <gideonnfrancis999@gmail.com>
2025-03-23 04:11:03 +0800

    trap and npc dissolve effect

---
+++ CarmicahEngine/CarmicahScriptCore/Source/Abilities/Dissolve.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carmicah
+{
+    public class Dissolve : Entity
+    {
+        public override void OnUpdate(float dt)
+        {
+            if (HasComponent<Animation>())
+            {
+                if (GetComponent<Animation>().IsAnimFinished())
+                {
+                    Destroy();
+                }
+            }
+        }
+    }
+}
