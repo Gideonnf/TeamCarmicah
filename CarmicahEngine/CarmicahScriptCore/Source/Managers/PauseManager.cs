52225cd985dd3022ab3d3a9efb045dbceeb8c827
Gideon Francis <gideonnfrancis999@gmail.com>
2025-03-25 05:09:23 +0800

    pause things

--- CarmicahEngine/CarmicahScriptCore/Source/Managers/PauseManager.cs
+++ CarmicahEngine/CarmicahScriptCore/Source/Managers/PauseManager.cs
@@ -42,12 +42,16 @@ namespace Carmicah
 
         public void Pause()
         {
+            // dont double pause
+
+            if (IsPaused) return;
             CMConsole.Log("Pausing game");
             IsPaused = true;
 
             // creating moving to button generic side
-
-            //PauseMenuEntity = CreateGameObject("Settings_Menu");
+            Entity settings = FindEntityWithName("Settings_Menu");
+            if (settings == null)
+                CreateGameObject("Settings_Menu");
             //QuitButtonEntity = CreateGameObject("GameClose_Button");
             //ResumeButtonEntity = CreateGameObject(ResumeButton);
         }
@@ -56,6 +60,12 @@ namespace Carmicah
         {
             CMConsole.Log("UnPausing game");
             IsPaused = false;
+            Entity settings = FindEntityWithName("Settings_Menu");
+            if (settings != null)
+            {
+                settings.As<UISliding>().SlideThenSD();
+            }
+
             if (PauseMenuEntity != null)
             {
                 PauseMenuEntity.Destroy();
